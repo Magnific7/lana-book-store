dbbc3feae07402eda9540bd75fa0fbec6897ed89
Magnific7 <mjones6944@gmail.com>
2022-11-22 10:15:52 +0200

    dockerize and deplot

--- bi_hub_project/settings.py
+++ bi_hub_project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&!2dfa96vi=+3p$9!*nd36rnsvc+m2zll8dpurqo0!sqco4@1b'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
