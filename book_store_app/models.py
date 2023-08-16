eeb80a8774e76f1521ebed02cc1cb9faeefbc848
Magnific7 <mjones6944@gmail.com>
2023-08-16 13:49:48 -0700

    models

--- book_store_app/models.py
+++ book_store_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    class Meta:
+        db_table = 'books'
+
+    image = models.ImageField(upload_to='books/', null=True, blank=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    author = models.CharField(max_length=100)
+    publication_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
--- book_store_app/views.py
+++ book_store_app/views.py
@@ -1,15 +1,8 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
 def Index(request):
-    return render(request, 'index.html', {})
+    books = Book.objects.all()
+    return render(request, 'index.html', {'books': books})
 
-def Analytic(request):
-    return render(request, 'analytics-dashboards.html', {})
-
-def reconciliation(request):
-    return render(request, 'Reconciliation.html', {})
-
-
-def CrosslingApp(request):
-    return render(request, 'cross-selling.html', {})
